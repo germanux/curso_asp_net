80a32e097100d1850347ce5dfd50b0a266097bc1
Germán Caballero <gcaballero@pronoide.es>
2017-10-13 17:42:28 +0200

    Ejemplos JavaScript, EcmaScrtipt 6, Ajax, CSS, jQuery.

---
+++ AppPrototipoFavoritos/AplicacionWeb/js/index.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(() => {
+    $("a[href='usuarios.html']").click((ev) => {
+        ev.preventDefault();
+        $("section").load("usuarios.html");
+    });
+    $("aside").click(() => {
+        $("aside").hide();
+    });
+    $("a").click( (infoEvento) => {
+        //infoEvento.preventDefault();
+        console.log("Se ha pulsado algo");
+    });
+
+    $("ul li").addClass("fondo-rojo");
+    setTimeout(() => {
+        $("ul li").removeClass("fondo-rojo");
+    }, 2000);
+});
--- AppPrototipoFavoritos/AplicacionWeb/js/usuario.js
+++ AppPrototipoFavoritos/AplicacionWeb/js/usuario.js
@@ -1,9 +1,37 @@
-﻿let nombre = window.localStorage.getItem("nombre");
-let nombreDOM = document.getElementById("nombre");
+﻿window.onload = function (ev) {
 
-if (nombre !== null || nombre !== "" ) {
-    nombreDOM.value = nombre;
+    let nombre = window.localStorage.getItem("nombre");
+    let nombreDOM = document.getElementById("nombre");
+    nombreDOM.onkeypress = function (ev) {
+        console.log(`Se ha pulsado una tecla ${nombre}` );
+    }
+    // nombreDOM.addEventListener("click", fu)
+    nombreDOM.onchange = function (ev) {
+        console.log("Se ha cambiado");
+    }
+    if (nombre !== null || nombre !== "") {
+        nombreDOM.value = nombre;
+    }
+    console.log("Log de lo que sea");
+    //console.error("ERROR GARRAFAL!");
+    //console.warn("ERROR  NO TAN GARRAFAL!");
+    document.getElementById("enviar").onclick = function () {
+        window.localStorage.setItem("nombre", nombreDOM.value);
+        document.getElementById("listadoUsuarios").innerHTML += "<li>Antonio</li>";
+    };
+    let string = JSON.stringify({
+
+        nombre: "Germán",
+        apellidos: "Caballero"
+    });
+    console.log(string);
+    let objeto = JSON.parse(string);
+    console.log(objeto);
+    setInterval( function () {
+        console.log((new Date).toString());
+    }, 10000);
+    miFun( "J", "X");
 }
-document.getElementById("enviar").onclick = function () {
-    window.localStorage.setItem("nombre", nombreDOM.value);
-}; 
+function miFun(v1 = "A", v2 = "b", v3 = "C") {
+    console.log(`${v1} ${v2} ${v3}`);
+}
