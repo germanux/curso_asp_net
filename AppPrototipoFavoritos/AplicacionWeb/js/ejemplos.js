a2148f9127af1c490b24348680b60425d63b8d26
Germán Caballero <gcaballero@pronoide.es>
2017-10-13 11:36:40 +0200

    Ejemplo web HTML, JS, CSS

---
+++ AppPrototipoFavoritos/AplicacionWeb/js/ejemplos.js
@@ -0,0 +1,35 @@
+﻿if (10 === "10") { alert("Mira tú!"); }
+
+function Usuario(nombre, email, edad) {
+    this.nombre = nombre;
+    this.email = email;
+    this.edad = edad;
+    this.metodo = function () {
+
+        alert("Cachondeooo!");
+    }
+    this.metodo2 = (para) => { alert("Cachondeooo 2!");; }
+} if (unaVariable == "undefined") {
+
+}
+Usuario.funEstatica = function () {
+    alert("Cachondeooo 3!");
+}
+usuario = new Usuario("Seat", 34);
+coche = {
+    nombre: "Seat",
+    ruedas: 34,
+    metodo: function () { return 0; }
+}
+coche.miOtroMetodo = function () { alert("Cachondeooo 4!"); }
+/*
+Usuario.funEstatica();
+usuario.metodo();
+usuario.metodo2();
+coche.miOtroMetodo();*/
+var nombre = document.getElementById("nombre");
+
+document.getElementById("enviar").onclick = function () {
+    //alert("No toques las ruedas " + nombre.value);
+    confirm("No preguntes cosas rarar, Adrés");
+} 
---
+++ AppPrototipoFavoritos/AplicacionWeb/js/usuario.js
@@ -0,0 +1,9 @@
+﻿let nombre = window.localStorage.getItem("nombre");
+let nombreDOM = document.getElementById("nombre");
+
+if (nombre !== null || nombre !== "" ) {
+    nombreDOM.value = nombre;
+}
+document.getElementById("enviar").onclick = function () {
+    window.localStorage.setItem("nombre", nombreDOM.value);
+}; 
